414e341e8ba03c07176bc04261dddacb0a255652
Jeffrey Walton <noloader@gmail.com>
2018-03-06 07:06:26 -0500

    Update head notes

--- sha256-2-p8.cxx
+++ sha256-2-p8.cxx
@@ -1,9 +1,8 @@
 /* sha256-p8.cxx - Power8 SHA extensions using C intrinsics  */
 /*   Written and placed in public domain by Jeffrey Walton   */
 
-/* sha256-2-p8.cxx roates working variables in the */
-/* callers instead of the SHA round function.      */
-/* Loop unrolling penalizes performance.           */
+/* sha256-2-p8.cxx rotates working variables in the callers instead of */
+/* the SHA round function. Loop unrolling penalizes performance.       */
 
 /* xlC -DTEST_MAIN -qarch=pwr8 -qaltivec sha256-p8.cxx -o sha256-p8.exe  */
 /* g++ -DTEST_MAIN -mcpu=power8 sha256-p8.cxx -o sha256-p8.exe           */

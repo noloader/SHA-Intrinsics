aa9cd42bd858d35c20d20a1c6389970710f8eb59
Pavel Kryukov <kryukov@frtk.ru>
2018-03-22 17:13:50 +0300

    Upload cpuid checker

---
+++ cpuid.c
@@ -0,0 +1,42 @@
+/**
+ * cpuid.c
+ *
+ * Checks if CPU has support of SHA instructions
+ *
+ * @author kryukov@frtk.ru
+ * @version 4.0
+ */
+
+#ifndef SILENT
+#include <stdio.h>
+#endif
+
+#if defined(__clang__) || defined(__GNUC__)
+
+#include <cpuid.h>
+static int CheckCPUsupportSHA()
+{
+    unsigned int CPUInfo[4];
+    __cpuid_count(7, 0, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
+    return CPUInfo[1] & (1 << 29); /* Check SHA */
+}
+
+#else /* defined(__clang__) || defined(__GNUC__) */
+
+static int CheckCPUsupportSHA()
+{
+    unsigned int CPUInfo[4];
+    __cpuidex(CPUInfo, 7, 0);
+    return CPUInfo[1] & (1 << 29); /* Check SHA */
+}
+
+#endif /* defined(__clang__) || defined(__GNUC__) */
+
+int main(int argc, char ** argv)
+{
+    const int res = !CheckCPUsupportSHA();
+#ifndef SILENT
+    printf("This CPU %s SHA-NI\n", res ? "does not support" : "supports");
+#endif
+    return res;
+}

c76931c89b0f86ea94028e046b04eb68fabf7ef6
Pavel I. Kryukov <kryukov@frtk.ru>
2018-03-23 11:44:57 +0300

    Add leaf checks to cpuid checks

--- cpuid.c
+++ cpuid.c
@@ -14,18 +14,26 @@
 #if defined(__clang__) || defined(__GNUC__)
 
 #include <cpuid.h>
-static int CheckCPUsupportSHA()
+int supports_sha_ni(void)
 {
     unsigned int CPUInfo[4];
+    __cpuid(0, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
+    if (CPUInfo[0] < 7)
+        return 0;
+
     __cpuid_count(7, 0, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
-    return CPUInfo[1] & (1 << 29); /* Check SHA */
+    return CPUInfo[1] & (1 << 29); /* SHA */
 }
 
 #else /* defined(__clang__) || defined(__GNUC__) */
 
-static int CheckCPUsupportSHA()
+int supports_sha_ni(void)
 {
     unsigned int CPUInfo[4];
+    __cpuid(CPUInfo, 0);  
+    if (CPUInfo[0] < 7)
+        return 0;
+
     __cpuidex(CPUInfo, 7, 0);
     return CPUInfo[1] & (1 << 29); /* Check SHA */
 }
